e160af5e0a20134e355c335d52b1e985f4f2797a
hojjat70 <hojjat.ramezani70@gmail.com>
2022-05-01 17:26:42 +0430

    doing Authorization

--- my-app/pages/authorization.tsx
+++ my-app/pages/authorization.tsx
@@ -6,18 +6,20 @@ const authorization = () =>
     return (
         <div className='pt-28 px-6 pb-6 bg-gradient-to-r from-cyan-500 to-blue-500 min-h-screen'>
 
+            <img width={100} className="block mx-auto" src="images/logo.png" alt="" />
+            <p className="text-center text-2xl mb-12 font-bold text-white">به توییتر خوش آمدید</p>
+
             <div className='w-1/4 mx-auto rounded-md bg-slate-50 overflow-hidden'>
                 <div className='flex border-b mb-2'>
-                    <div className='basis-1/2 py-4 text-center cursor-pointer hover:bg-slate-100'>Login</div>
-                    <div className='basis-1/2 py-4 text-center cursor-pointer hover:bg-slate-100 border-l'>Signup</div>
+                    <div className='basis-1/2 py-4 text-center cursor-pointer hover:bg-slate-100 '>ورود</div>
+                    <div className='basis-1/2 py-4 text-center cursor-pointer hover:bg-slate-100 border-l'>ثبت نام</div>
                 </div>
 
                 <div>
                     <Signup />
                 </div>
-
-
             </div>
+
         </div>
     );
 };
